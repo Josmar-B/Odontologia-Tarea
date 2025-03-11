b8021f1c0b1f3b8eb8b7976c6a9f714481d08b13
Josmar-B <josmar321j@gmail.com>
2025-03-10 23:13:18 -0400

    Update settings.py

--- odontologia_project/settings.py
+++ odontologia_project/settings.py
@@ -13,9 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-ALLOWED_HOSTS = [
-*     # Para desarrollo local
-]
+ALLOWED_HOSTS = [*]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

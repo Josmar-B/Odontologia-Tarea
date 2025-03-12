21b980b9a380adacf91c7975fecc2a0eeabfe3a1
Josmar-B <josmar321j@gmail.com>
2025-03-12 10:15:48 -0400

    Update forms.py

--- odontologia_app/forms.py
+++ odontologia_app/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from .models import *
+
+class RepresentanteForm(forms.ModelForm):
+    class Meta:
+        model = Representante
+        fields = ['cedula', 'nombre']
+
 class RegistroForm(UserCreationForm):
     email = forms.EmailField(required=True)
     nombre = forms.CharField(max_length=100, required=True)
